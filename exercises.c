deb84020157b93a8f487dbeea95d38da9611b1ed
tup32132 <kyle.kinney@temple.edu>
2023-01-22 17:46:23 -0500

    create notes for 2.4 and 2.5, exercise 2.3

--- exercises.c
+++ exercises.c
@@ -1,15 +1,34 @@
 #include <stdio.h>
+#define PI 3.14159
 
-/* 2.2 exercises*/
-int main ()
+/* 2.3 exercises*/
+
+/*int main ()
 {
     int first;
     int second;
     int third;
-    printf("Please provide three integers: \n");
+    printf("Please provide three integers and type done when complete: \n");
     scanf("%d%d%d\n", &first, &second, &third);
 
-    printf( "Are these correct?\n %d, %d, %d", first, second, third);
+    printf( "Are these correct?\n %d, %d, %d\n", first, second, third);
+
+    return(0);
+} */
+
+/*int main ()
+{
+    double radius;
+
+    printf("Please provide the radius of the circle: \n");
+    scanf( "%lf", &radius);
+
+    double area;
+
+    area = PI * radius * radius;
+
+    printf("The area of the circle is %f.\n", area);
 
     return(0);
 }
+*/
